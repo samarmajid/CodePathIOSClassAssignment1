1433f891ad83e3ce50f7da793bf8408dcadad769
Samar Majid <summermajid@gmail.com>
2022-02-27 19:29:29 -0500

    Initial Commit

---
+++ CodePathiOSClassAssignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodePathiOSClassAssignment1
+//
+//  Created by Samar Majid on 2/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
